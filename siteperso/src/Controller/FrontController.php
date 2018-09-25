92c7f512b2b38850755d495201ab5319f654a5f4
Paulin Saint-Clivier <p.stclivier@gmail.com>
2018-09-25 09:58:23 +0200

    theme css

--- siteperso/src/Controller/FrontController.php
+++ siteperso/src/Controller/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,11 +19,40 @@ class FrontController extends AbstractController
     }
 
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home_page")
      */
     public function home(){
         return $this->render('front/home.html.twig', [
             'title' =>'bienvenue'
         ]);
+    }
+
+    /**
+     * @Route("/paulinsc/competences", name="skillz_page")
+     */
+    public function skills(){
+        return $this->render('front/skills.html.twig', [
+            'title' =>'bienvenue'
+        ]);
+    }
+
+    /**
+     * @Route("/paulinsc/porfolio", name="porfolio_page")
+     */
+    public function porfolio(){
+        return $this->render('front/porfolio.html.twig', [
+            'title' =>'bienvenue'
+        ]);
+    }
+
+    /**
+     * @Route("/paulinsc/contact", name="contact_page")
+     */
+    public function contact(){
+        $form = $this->createForm(ContactType::class);
+        return $this->render('front/contact.html.twig', [
+            'formContact' => $form->createView(),
+        ]);
+
     }
 }
